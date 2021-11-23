ec0f94711896d8839d1885eb1ea1c713631663ab
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-24 06:44:27 +0800

    last controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,15 @@
 const router = require('express')
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
