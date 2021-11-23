a10efea8d9aaf5763be810a84933c97403fa931d
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-24 06:43:21 +0800

    done dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -21,3 +21,23 @@ router.get('/new', withAuth, (req, res) => {
     res.render('new-post', { loggedIn: true });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: { id: req.params.id },
+        attributes: ['id', 'post_content', 'title', 'created_at']
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
