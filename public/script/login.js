b58b0e821375978cc90785996cc689e1175475d4
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-26 00:25:54 +0800

    login js frontend

---
+++ public/script/login.js
@@ -0,0 +1,25 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
