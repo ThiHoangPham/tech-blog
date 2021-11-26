142f77c641db82575c70b563dd356c56f59b991b
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-27 02:25:34 +0800

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const helpers = require('./utils/helpers');
 
 const app = express();
 
-const PORT = process.env.PORT || '8080';
+const PORT = process.env.PORT || 8080;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
