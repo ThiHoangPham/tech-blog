c2a73b8e54c7073d2b0dff981c72d8500a37ecc6
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-26 23:24:27 +0800

    done edit-post frontend

---
+++ public/script/edit-post.js
@@ -0,0 +1,23 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/').pop();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('textarea[name="post-content"]').value;
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, post_content }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
