742de4033125bc07f5424efd84042128e9724812
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-28 01:13:26 +0800

    push heroku

--- server.js
+++ server.js
@@ -33,6 +33,5 @@ app.set('view engine', 'handlebars');
 const routes = require('./controllers');
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
+
+app.listen(PORT, () => console.log('Now listening'));
